25ff9656e162b5dd556be9f5ac9cfb522e9ed51e
Franco Pérez <franlepeva@gmail.com>
2026-02-16 15:45:08 -0300

    script.js: Añadidas clase Tarea con propiedades id, descripción, estado y fecha de creación y método para cambiar el estado, y clase GestoTareas con propiedades list, un arreglo de objetos que almacenará las tareas, y métodos para agregar, eliminar y mostrar las tareas. Se añadieron ejemplos de instanciación de ambas clases y uso de métodos para agregar las tareas instanciadas a la lista de tareas

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("Bienvenidos!");
+
+class Tarea {
+    constructor(id,desc,estado,fechaCreacion) {
+        this.id = id;
+        this.desc = desc;
+        this.estado = estado;
+        this.fechaCreacion = fechaCreacion;
+    }
+    cambiarEstado(nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+}
+
+class GestorTareas {
+    constructor() {
+        this.lista = [];
+    }
+    agregarTarea(nuevaTarea) {
+        this.lista.push(nuevaTarea);
+    }
+    eliminarTarea(id) {
+        this.lista.splice(id-1,1);
+    }
+    mostrarLista() {
+        return this.lista;
+    }
+}
+var listaTareas = new GestorTareas();
+
+var tarea1 = new Tarea(1,"Comprar papas fritas","pendiente","16/2/2026");
+listaTareas.agregarTarea(tarea1);
+
+var tarea2 = new Tarea(2,"Lavar loza","pendiente","17/2/2026");
+listaTareas.agregarTarea(tarea2);
