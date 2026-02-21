40ef9a7558e9049654556b9a81f72a4c68cbe8bf
Franco Pérez <franlepeva@gmail.com>
2026-02-21 19:49:11 -0300

    Se mejoró apariencia de formulario y tarjetas, mejorando estilos como ancho de elementos, alineación vertical, posición, diseño y comportamiento de interfaz en general. En index.html, se agregó campo de fecha para formulario. En script.js se añadió propiedad en clase Tarea que almacena la fecha límite. Se añadieron funciones setTimeout con un retraso de 2 segundos para los alert de tarea agregada y estado modificado. Se reimplementó el ingreso de las tareas al arreglo de tareas mediante spread. Se añadió la captura de la fecha límite ingresada en el listener del formulario. En función mostrarTarea() se añadió destructuring para extraer variables del objeto tarea y se optimizó el código html de la tarjeta bootstrap. Se añadió condicional para detectar si se ingresó fecha límite y mostrar el elemento de conteo en caso positivo, y a su vez llamar a la nueva función startContador() para ejecutar una cuenta regresiva y actualizar el conteo en la tarjeta de la tarea correspondiente, utilizando un setInterval(). Añadido destructuring en listener de tarjetas y mejoras en los estilos añadidos al DOM dinámicamente en el código en general.

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 console.log("Bienvenidos!");
 
 class Tarea {
-    constructor(id,titulo,desc,estado,fechaCreacion) {
+    constructor(id,titulo,desc,estado,fechaCreacion,limite) {
         this.id = id;
         this.titulo = titulo
         this.desc = desc;
         this.estado = estado;
         this.fechaCreacion = fechaCreacion;
+        this.limite = limite;
     }
     cambiarEstado = () => {
         if (this.estado == "pendiente") {
             this.estado = "completada";
         }
         tarjetaEstado(this.id);
-        alert(`La tarea ${this.titulo} se ha marcado como "completada"`);
+        setTimeout(alert(`La tarea "${this.titulo}" se ha marcado como "completada"`),2000);
     }
 }
 
@@ -22,9 +23,12 @@ class GestorTareas {
         this.lista = [];
     }
     agregarTarea = nuevaTarea => {
-        this.lista.push(nuevaTarea);
-        mostrarTarea(nuevaTarea);
-    }
+        setTimeout(()=>{
+            this.lista = [...this.lista,nuevaTarea];
+            mostrarTarea(nuevaTarea);
+            alert(`Tarea "${nuevaTarea.titulo}" registrada`);
+        },2000)
+    };
     eliminarTarea = idtarea => {
         // Se busca el índice del arreglo en el que está la tarea con el id deseado
         let idx = this.lista.findIndex(check);
@@ -34,6 +38,7 @@ class GestorTareas {
         // Se elimina el objeto tarea en el índice correspondiente
         this.lista.splice(idx,1);
         quitarTarjeta(idtarea);
+        alert(`La tarea "${this.titulo}" se ha eliminado`);
     }
     mostrarLista = () => {
         console.log(this.lista);
@@ -49,8 +54,10 @@ document.getElementById("add-form").addEventListener('submit', (ev) => {
     ev.preventDefault();
     let in_titulo = document.getElementById("title").value;
     let in_desc = document.getElementById("descr").value;
+    let in_limite = new Date(document.getElementById("limite").value); // Captura fecha límite ingresada
+    let fecha_creacion = new Date(); // Crea objeto Date con la fecha y tiempo de ingreso realizado
 
-    let tarea = new Tarea(id_acc,in_titulo,in_desc,"pendiente","16/2/2026");
+    let tarea = new Tarea(id_acc,in_titulo,in_desc,"pendiente",fecha_creacion,in_limite);
     listaTareas.agregarTarea(tarea);
     //listaTareas.mostrarLista();
 
@@ -62,37 +69,32 @@ document.getElementById("add-form").addEventListener('submit', (ev) => {
 // Se llama desde método listaTareas.agregarTarea()
 
 function mostrarTarea(tarea) {
-    let card = `<div id="tarea-${tarea.id}" class="col-12 my-2 col-lg-6">
-    <div class="card text-center align-self-center m-0 p-0 rounded-5">
-        <div class="card-body row">
-            <h5 class="card-title col-6">${tarea.titulo}</h5>
-            <p class="card-text col-6">${tarea.desc}</p>
-            <h6 class="card-subtitle mb-2 text-body-secondary col-6">Fecha de creación: ${tarea.fechaCreacion}</h6>
-            <p id="estado-${tarea.id}" class="card-text text-danger col-6">Pendiente</p>
-        </div>
-        <button id="btn-term-${tarea.id}" class="btn btn-primary col-4 m-3 rounded-5">Terminada</button>
-        <button id="btn-elim-${tarea.id}" class="btn btn-danger col-4 m-3 rounded-5">Elminar</button>
-    </div>
-    </div>`;
+    let {id,titulo,desc,fechaCreacion,limite} = tarea;
+    let card = `<div id="tarea-${id}" class="align-self-center my-2 col-12 col-lg-6"><div class="card text-center align-self-center m-0 p-0 rounded-5"><div class="card-body row"><div class="col-12 col-md-6 align-self-center"><h5 class="card-title w-100 col-6">${titulo}</h5><p class="card-text border border-1 rounded-4 w-100 p-1 col-6">${desc}</p></div><div class="col-12 col-md-6 align-self-center"><h6 id="estado-${id}" class="card-text text-danger w-100 my-1 col-6">Pendiente</h6><h6 class="card-subtitle text-body-secondary w-100 my-1 col-6">Creada: ${fechaCreacion.toLocaleString()}</h6></div></div><div class ="col-12"><button id="btn-term-${id}" class="btn btn-primary w-25 m-1 rounded-5">Terminar</button><button id="btn-elim-${id}" class="btn btn-danger w-25 m-1 rounded-5">Elminar</button></div></div>`;
     document.getElementById("show").innerHTML += card;
+    if (limite) {    
+        let padre = document.getElementById(`tarea-${id}`).firstChild.firstChild.lastChild;
+        let el = document.createElement("h6");
+        el.setAttribute("class","w-100 text-success col-6");
+        
+        startContador(fechaCreacion,limite,padre,el);
+    }
 }
 
 // Captura de botón
 
 document.getElementById("show").addEventListener("click",(ev)=>{
-    let elem = ev.target;
-    if (elem.tagName === 'BUTTON') {
-        console.log(ev.target);
-        let elid = elem.id.split("-")[2];
-        let eltype = elem.id.split("-")[1];
+    let {target} = ev;
+    if (target.tagName === 'BUTTON') {
+        console.log(target);
+        let elid = target.id.split("-")[2];
+        let eltype = target.id.split("-")[1];
         switch (eltype) {
             case "elim":
-                alert(`Botón eliminar apretado en tarea: ${elid}`);
                 listaTareas.eliminarTarea(elid);
                 console.log(listaTareas.lista);
                 break;
             case "term":
-                alert(`Botón terminar apretado en tarea: ${elid}`);
                 listaTareas.lista.forEach(el=>{
                     if (el.id == elid) {
                         el.cambiarEstado();
@@ -115,6 +117,30 @@ function quitarTarjeta(id) {
 function tarjetaEstado(id) {
     let tarj = document.getElementById(`estado-${id}`);
     tarj.innerHTML = "Completada";
-    tarj.setAttribute("class","card-text text-success col-6");
+    tarj.setAttribute("class","card-text text-success w-100 my-1 col-6");
 }
 
+// Cuenta regresiva
+
+function startContador(creacion,limite,padre,elem) {
+    limite = limite.getTime();// Obtenemos la fecha en milisegundos
+    creacion = creacion.getTime();
+    let difer = limite-creacion;// Restamos y nos da el tiempo de inicio para la cuenta
+
+    let timer = setInterval(()=>{
+        let dias = Math.floor(difer/(1000*60*60*24));
+        let horas = Math.floor((difer%(1000*60*60*24))/(1000*60*60));
+        let minutos = Math.floor((difer%(1000*60*60))/(1000*60));
+        let segundos = Math.floor((difer%(1000*60))/(1000));
+        
+        elem.innerText = `Queda: ${dias} D ${horas} Hr ${minutos} Min ${segundos} S`; // Mostrar días, horas, minutos y segundos
+        padre.appendChild(elem);// Mostrar elemento en el documento
+        
+        difer = difer-1000;// Restar un segundo
+        if (difer < 0) {
+            clearInterval(timer);
+            elem.setAttribute("class","w-100 text-danger col-6")
+            elem.innerText = "Límite Expirado";
+        }
+    },1000);
+}
